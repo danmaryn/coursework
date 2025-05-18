72f39d285758911385168e2f7950912baa0d4a2f
danmaryn <marynenko.danylo@lll.kpi.ua>
2025-05-18 05:33:09 +0300

    add algorithms and print

--- main.cpp
+++ main.cpp
@@ -21,6 +21,92 @@ void Filling_An_Map_Check(Map *map, const int *height, const int *width)
     }
 }
 
+void Check_Columns_Down(Map *map, const int *height, const int *width)
+{
+    for (int j = 0; j < *width; j++)
+    {
+        for (int i = *height - 1; i >= 0; i--)
+        {
+            for (int k = i - 1; k >= 0; k--)
+            {
+                if (map->map_number[i][j] == map->map_number[k][j] && !map->map_check[k][j])
+                {
+                    map->map_check[i][j] = true;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+void Check_Columns_Up(Map *map, const int *height, const int *width)
+{
+    for (int j = 0; j < *width; j++)
+    {
+        for (int i = 0; i < *height; i++)
+        {
+            for (int k = i + 1; k < *height; k++)
+            {
+                if (map->map_number[i][j] == map->map_number[k][j] && !map->map_check[k][j])
+                {
+                    map->map_check[i][j] = true;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+void Check_Rows(Map *map, const int *height, const int *width)
+{
+    for (int i = 0; i < *height; i++)
+    {
+        for (int j = 0; j < *width; j++)
+        {
+            for (int k = 0; k < *width; k++)
+            {
+                if (map->map_number[i][j] == map->map_number[i][k] && (j != k) && (!map->map_check[i][k]))
+                {
+                    map->map_check[i][j] = true;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+void Check_Map(Map *map, const int *height, const int *width)
+{
+    for (int i = 0; i < *height; i++)
+    {
+        for (int j = 0; j < *width-1; j++)
+        {
+            if (map->map_check[i][j] == map->map_check[i][j + 1])
+            {
+                map->map_check[i][j] = false;
+            }
+        }
+    }
+
+    for (int j = 0; j < *width; j++)
+    {
+        for (int i = *height - 1; i >= 0; i--)
+        {
+            if (map->map_check[j][i] == map->map_check[j-1][i])
+            {   
+                map->map_check[j-1][i] = false;
+                for (int k = *height -1; k >= 0; k-- )
+                {
+                    if ( map->map_number[j-1][i] == map->map_number[k][i] && (k != (j-1)))
+                    {
+                        map->map_check[k][i] = true;
+                    }
+                }
+            }
+        }
+    }
+}
+
 int main()
 {
     Map map = {
