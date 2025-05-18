fb8c045ac6a5acbd88486c27108be52dff3f27df
danmaryn <marynenko.danylo@lll.kpi.ua>
2025-05-18 05:28:06 +0300

    add map

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
-#include <iostream>
+ #include <iostream>
+using namespace std;
 
-int main(int argc, char const *argv[])
+#define MAP_WIDTH 6
+#define MAP_HEIGHT 6
+
+struct Map
 {
-    while (true)
-    {
-        
-    }
-    
+    int map_number[MAP_HEIGHT][MAP_WIDTH];
+    bool map_check[MAP_HEIGHT][MAP_WIDTH];
+};
+
+int main()
+{
+    Map map = {
+        {{6, 6, 3, 2, 1, 5},
+         {1, 2, 6, 3, 5, 4},
+         {5, 1, 4, 2, 3, 4},
+         {3, 3, 3, 5, 2, 1},
+         {4, 2, 1, 6, 5, 3},
+         {6, 1, 5, 4, 4, 4}},
+        {}};
+
     return 0;
 }
